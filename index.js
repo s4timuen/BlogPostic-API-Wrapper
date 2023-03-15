b5ba1bad6fd9b9afdffc74e7219174742689e9d1
s4timuen <tim.muenzinger@yahoo.de>
2023-03-15 12:11:12 +0100

    added blog routes

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ export default class ApiWrapper {
         return fetchResource('users', {
             method: 'GET',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             }
         });
     }
@@ -126,7 +126,7 @@ export default class ApiWrapper {
         return fetchResource(`users/${userId}`, {
             method: 'GET',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             }
         });
     }
@@ -135,7 +135,7 @@ export default class ApiWrapper {
         return fetchResource('users', {
             method: 'GET',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             },
             body: {
                 firstName,
@@ -151,7 +151,7 @@ export default class ApiWrapper {
         return fetchResource(`users/${userId}`, {
             method: 'PATCH',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             },
             body: {
                 ...data
@@ -163,7 +163,7 @@ export default class ApiWrapper {
         return fetchResource(`users/${userId}`, {
             method: 'DELETE',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             }
         });
     }
@@ -172,7 +172,7 @@ export default class ApiWrapper {
         return fetchResource('users/me', {
             method: 'GET',
             headers: {
-                Authorization: `Bearer ${this.jwt}`,
+                Authorization: `Bearer ${this.jwt}`
             }
         });
     }
@@ -244,6 +244,94 @@ export default class ApiWrapper {
 
     updateMyData(data) {
         return fetchResource('users/update-my-data', {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
+
+    ////////// Blog Requests //////////
+    getBlog(blogId) {
+        return fetchResource(`blogs/blog/${blogId}`, {
+            method: 'GET',
+            headers: {}
+        });
+    }
+
+    getAllBlogs() {
+        return fetchResource('blogs', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`
+            }
+        });
+    }
+
+    getAllMyBlogs() {
+        return fetchResource('blogs/my-blogs', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    getAllBlogsUser(userId) {
+        return fetchResource(`blogs/user/${userId}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    createBlog(author, title, description, tags) {
+        return fetchResource('blogs/create-blog', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: {
+                author,
+                title,
+                description,
+                tags
+            }
+        });
+    }
+
+    updateMyBlog(blogId, data) {
+        return fetchResource(`blogs/update-my-blog/${blogId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
+
+    deleteMyBlog(blogId) {
+        return fetchResource(`blogs/delete-my-blog/${blogId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    deleteBlog(blogId) {
+        return fetchResource(`blogs/blog/${blogId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    updateBlog(blogId, data) {
+        return fetchResource(`blogs/blog/${blogId}`, {
             method: 'PATCH',
             headers: {
                 Authorization: `Bearer ${this.jwt}`,
