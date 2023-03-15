94fb79e5b758720ce15acc1697b8794a60970189
s4timuen <tim.muenzinger@yahoo.de>
2023-03-15 15:37:13 +0100

    added articles requests

--- index.js
+++ index.js
@@ -339,4 +339,96 @@ export default class ApiWrapper {
             body: { ...data }
         });
     }
+
+    ////////// Article Requests //////////
+    getArticle(articleId) {
+        return fetchResource(`articles/article/${articleId}`, {
+            method: 'GET',
+            headers: {}
+        });
+    }
+
+    getAllArticles() {
+        return fetchResource('articles', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`
+            }
+        });
+    }
+
+    getAllMyArticles() {
+        return fetchResource('articles/my-articles', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    getAllArticlesUser(userId) {
+        return fetchResource(`articles/user/${userId}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    createArticle(author, blog, title, content, tags, links, attachements, attachmentMimeTypes) {
+        return fetchResource('articles/create-article', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: {
+                author,
+                blog,
+                title,
+                links,
+                content,
+                tags,
+                attachements,
+                attachmentMimeTypes
+            }
+        });
+    }
+
+    updateMyArticle(articleId, data) {
+        return fetchResource(`articles/update-my-article/${articleId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
+
+    deleteMyArticle(articleId) {
+        return fetchResource(`articles/delete-my-article/${articleId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    deleteArticle(articleId) {
+        return fetchResource(`articles/article/${articleId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    updateArticle(articleId, data) {
+        return fetchResource(`articles/article/${articleId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
 }
