bbd2d4b86dc0918af1b1e417061ebe666517c8ed
s4timuen <tim.muenzinger@yahoo.de>
2023-03-24 17:37:01 +0100

    added post functions and fixed two article functions

--- index.js
+++ index.js
@@ -375,8 +375,8 @@ export default class ApiWrapper {
         });
     }
 
-    createArticle(author, blog, title, content, tags, links, attachements, attachmentMimeTypes) {
-        return fetchResource('articles/create-article', {
+    createArticle(author, blog, title, content, tags, links, attachements, attachmentsMimeTypes) {
+        return fetchResource('articles', {
             method: 'POST',
             headers: {
                 Authorization: `Bearer ${this.jwt}`,
@@ -389,7 +389,7 @@ export default class ApiWrapper {
                 content,
                 tags,
                 attachements,
-                attachmentMimeTypes
+                attachmentsMimeTypes
             }
         });
     }
@@ -431,4 +431,98 @@ export default class ApiWrapper {
             body: { ...data }
         });
     }
+
+    ////////// Post Requests //////////
+    getPost(postId) {
+        return fetchResource(`posts/post/${postId}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`
+            }
+        });
+    }
+
+    getAllPosts() {
+        return fetchResource('posts', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`
+            }
+        });
+    }
+
+    getAllMyPosts() {
+        return fetchResource('posts/my-posts', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    getAllPostsUser(userId) {
+        return fetchResource(`posts/user/${userId}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    createPost(author, title, content, tags, attachement, attachmentMimeType) {
+        return fetchResource('posts', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: {
+                author,
+                title,
+                content,
+                tags,
+                attachement,
+                attachmentMimeType
+            }
+        });
+    }
+
+    updateMyPost(postId, data) {
+        return fetchResource(`posts/update-my-post/${postId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
+
+    deleteMyPost(postId) {
+        return fetchResource(`posts/delete-my-post/${postId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    deletePost(postId) {
+        return fetchResource(`posts/post/${postId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            }
+        });
+    }
+
+    updatePost(postId, data) {
+        return fetchResource(`posts/post/${postId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+            },
+            body: { ...data }
+        });
+    }
+
+    ////////// Comment Requests //////////
 }
