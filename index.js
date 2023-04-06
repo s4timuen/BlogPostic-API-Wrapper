46e8edef05efe6b6c55b56d4a86cfa8084267e81
s4timuen <tim.muenzinger@yahoo.de>
2023-04-06 16:56:25 +0200

    fixed import paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import user from './components/user';
-import blog from './components/blog';
+import user from './components/endpoints/user';
+import blog from './components/endpoints/blog';
 import article from './components/endpoints/article';
-import post from './components/post';
-import comment from './components/comment';
+import post from './components/endpoints/post';
+import comment from './components/endpoints/comment';
 
 
 export default class ApiWrapper {
